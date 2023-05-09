2d94e27d76645fe368286242b3a7d209ac24151f
alexowoade <alexowoade@gmail.com>
2023-05-09 21:12:03 +0000

    alex

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,27 +1,8 @@
 #!/usr/bin/env python3
-"""This module contains a function that queries a MongoDB collection
-and returns the students sorted by their average score using pymongo."""
-
-from typing import List
-from pymongo.collection import Collection
-
-
-def top_students(mongo_collection: Collection) -> List[dict]:
-    """Returns all students sorted by average score.
-
-    Args:
-        mongo_collection (Collection): The MongoDB collection to query.
-
-    Returns:
-        List[dict]: A list of dictionaries representing the students and their average scores.
-
-    Raises:
-        TypeError: If mongo_collection is not a Collection object.
-    """
-    if not isinstance(mongo_collection, Collection):
-        raise TypeError("mongo_collection must be a Collection object")
-
-    students = mongo_collection.aggregate([
+""" MongoDB Operations with Python using pymongo """
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score """
+    top_st = mongo_collection.aggregate([
         {
             "$project": {
                 "name": "$name",
@@ -30,4 +11,4 @@ def top_students(mongo_collection: Collection) -> List[dict]:
         },
         {"$sort": {"averageScore": -1}}
     ])
-    return list(students)
+    return top_st
