1211c1e768869ca6aa1fadd89978057b91d0eb67
alexowoade <alexowoade@gmail.com>
2023-05-09 20:48:20 +0000

    alex

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 """ MongoDB Operations with Python using pymongo """
+
 def schools_by_topic(mongo_collection, topic):
-    """ returns the list of school having a specific topic """
+    """ Returns the list of schools having a specific topic.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The collection of schools.
+        topic (str): The topic to filter by.
+
+    Returns:
+        list: The list of schools that have the topic in their topics field.
+    """
     documents = mongo_collection.find({"topics": topic})
-    list_docs = [d for d in documents]
+    list_docs = [doc for doc in documents]
     return list_docs
